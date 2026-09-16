d8ef24532f802f1ab86e41ca2680f17f8b0bb396
Diogo02acai <diogo.ribeiro@escolar.ifrn.edu.br>
2026-09-16 16:10:14 -0300

    fix: corrige bugs

--- src/main/java/com/example/demo/service/TarefaService.java
+++ src/main/java/com/example/demo/service/TarefaService.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
 
+import com.example.demo.controller.DTO.TaskRequestDTO;
+import com.example.demo.controller.DTO.TaskResponseDTO;
 import com.example.demo.model.Tarefa;
 import com.example.demo.repository.TarefaRepository;
 import org.springframework.stereotype.Service;
@@ -7,11 +9,17 @@ import java.util.List;
 
 @Service
 public class TarefaService {
+
     private final TarefaRepository repository;
+
     public TarefaService(TarefaRepository repository) {
         this.repository = repository;
     }
-    public Tarefa criar(String titulo) {
+
+    public TaskResponseDTO criar(TaskRequestDTO dto) {
+        String titulo = TarefaDTO.titulo();
+        Tarefa tarefa = new Tarefa((long)1.0,dto.titulo(),dto.descricao(),null, false);
+        return toResponseDTO(salvar);
         System.out.println("[SERVICE] Validando regra de negócio para: " +
                 titulo);
         if (titulo == null || titulo.isBlank()) {
@@ -19,13 +27,29 @@ public class TarefaService {
         }
         return repository.salvar(titulo.trim());
     }
-    public List<Tarefa> listar() {
+
+    public List<TaskResponseDTO> listarTodas() {
         System.out.println("[SERVICE] Solicitando lista de tarefas ao repository");
-        return repository.listarTodas();
+        return repository.listarTodas().stream().map(this::toReponseDTO).toList();
     }
+
     public Tarefa buscarPorId(Long id) {
         System.out.println("[SERVICE] Processando busca por id: " + id);
         return repository.buscarPorId(id)
                 .orElseThrow(() -> new IllegalArgumentException("Tarefa não encontrada: " + id));
     }
+
+    public List<Tarefa> listarConcluidos(){
+        System.out.println("[SERVICE] solicitando lista de tarefas concluidas");
+        List<Tarefa> tarefas = repository.listarTodas();
+        List<Tarefa> tarefasConcluidas=new List<Tarefa>()
+
+        for(tarefa in tarefas){
+            if(tarefa.isConcluido()){
+             tarefasConcluidas.add(tarefa);
+            }
+        }
+        return tarefasConcluidas;
+    }
+
 }
